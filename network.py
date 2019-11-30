a13dcc59170c3c3a68f8f6ed3bd69f2d78565369
Jinde Yang <jinde.yang97@gmail.com>
2019-11-30 16:06:15 -0600

    network update add cartpole experiment

--- network.py
+++ network.py
@@ -21,7 +21,10 @@ class QNetworkAtari(nn.Module):
         )
 
         # Fully connected: 64 * 7 * 7 = 3136 -> 512 -> num_action
-        self.fc_1 = nn.Linear(64 * 7 * 7, 512)
+        self.fc_1 = nn.Sequential(
+            nn.Linear(64 * 7 * 7, 512),
+            nn.ReLU(True)
+        )
         self.fc_2 = nn.Linear(512, env.action_space.n)
 
     def forward(self, state_in):
@@ -34,5 +37,29 @@ class QNetworkAtari(nn.Module):
         return q_out
 
 
+class QNetworkMlp(nn.Module):
+    def __init__(self, env: gym.Env):
+        super(QNetworkMlp, self).__init__()
+        self.fc_1 = nn.Sequential(
+            nn.Linear(env.observation_space.shape[0], 64),
+            nn.ReLU(True)
+        )
+        self.fc_2 = nn.Sequential(
+            nn.Linear(64, 64),
+            nn.ReLU(True)
+        )
+        self.fc_3 = nn.Linear(64, env.action_space.n)
+
+    def forward(self, state_in):
+        q_out = self.fc_1(state_in)
+        q_out = self.fc_2(q_out)
+        q_out = self.fc_3(q_out)
+        return q_out
+
+
 def q_network_atari_creator(env: gym.Env):
     return QNetworkAtari(env)
+
+
+def q_network_mlp_creator(env: gym.Env):
+    return QNetworkMlp(env)
