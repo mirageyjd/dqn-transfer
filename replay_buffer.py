71c5c0bf26681566cf307b917a5bb5b7874eb85b
Jinde Yang <jinde.yang97@gmail.com>
2019-11-30 00:00:31 -0600

    Replay buffer update: 1. Add type map from numpy to torch 2. Change interface of insert 3. Always use cpu tensor

--- replay_buffer.py
+++ replay_buffer.py
@@ -3,6 +3,13 @@ import torch
 import numpy as np
 import gym
 
+TYPE_MAP = {
+    np.uint8: torch.uint8,
+    np.int32: torch.int32,
+    np.int64: torch.int64,
+    np.float32: torch.float32,
+}
+
 
 class ReplayBuffer(object):
     def __init__(self, env: gym.Env, config: dict):
@@ -13,29 +20,47 @@ class ReplayBuffer(object):
             last: index of last inserted transition
             s, a, r, s2 : transition data
         """
-        self.device = config['device']
 
         self.n = 0
         self.capacity = config['replay_capacity']
         self.last = -1
 
-        self.s = torch.empty((self.capacity,) + env.observation_space.shape, device=self.device)
-        self.a = torch.empty(self.capacity, dtype=torch.long, device=self.device)
-        self.r = torch.empty(self.capacity, device=self.device)
-        self.s2 = torch.empty((self.capacity,) + env.observation_space.shape, device=self.device)
+        self.s = torch.empty((self.capacity,) + env.observation_space.shape,
+                             dtype=TYPE_MAP[env.observation_space.dtype])
+        self.a = torch.empty(self.capacity, dtype=TYPE_MAP[env.action_space.dtype])
+        self.r = torch.empty(self.capacity, dtype=torch.float32)
+        self.s2 = torch.empty((self.capacity,) + env.observation_space.shape,
+                              dtype=TYPE_MAP[env.observation_space.dtype])
 
-    # insert a transition tuple (s, a, r, s2)
-    def insert(self, trans: Tuple[torch.Tensor, int, float, torch.Tensor]):
+    def insert(self, trans: Tuple[np.ndarray, int, float, np.ndarray]):
+        """
+            insert a transition tuple
+
+            params:
+                trans: transition (s, a, r, s2)
+        """
+
         self.last = (self.last + 1) % self.capacity
         if self.n < self.capacity:
             self.n += 1
 
         self.last = (self.last + 1) % self.capacity
-        temp1, self.a[self.last], self.r[self.last], temp2 = trans
-        self.s[self.last] = torch.from_numpy(temp1).float().to(self.device)
-        self.s2[self.last] = torch.from_numpy(temp2).float().to(self.device)
+        s_new, a_new, r_new, s2_new = trans
+        self.s[self.last] = torch.from_numpy(s_new)
+        self.a[self.last] = a_new
+        self.r[self.last] = r_new
+        self.s2[self.last] = torch.from_numpy(s2_new)
 
-    # sample a batch
     def sample(self, batch_size: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+            sample a batch from replay buffer
+
+            params:
+                batch_size: the size of mini-batch
+
+            returns:
+                a tuple of transition batch (s_batch, a_batch, r_batch. s2_batch)
+        """
+
         ind = np.random.randint(0, self.n, size=batch_size)
         return self.s[ind], self.a[ind], self.r[ind], self.s2[ind]
