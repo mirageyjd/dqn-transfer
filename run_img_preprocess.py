1a8d9a33345e8f1b401e94a9a20168634345543d
Shiyu <shiyuwu@umich.edu>
2019-12-03 23:19:45 -0500

    UNIT-GAN: Build new datasets

--- run_img_preprocess.py
+++ run_img_preprocess.py
@@ -7,8 +7,8 @@ import shutil
 import numpy as np
 
 
-TENNIS_IMG_DIR = './datasets/atari/testA/'
-PONG_IMG_DIR = './datasets/atari/testB/'
+TENNIS_IMG_DIR = './tennis_img/'
+PONG_IMG_DIR = './pong_img/'
 OUTPUT_DIR = './temp/'
 
 
@@ -29,7 +29,7 @@ def rotate_pong_img(pong_img_dir):
         transposed.save(pong_img_dir + file, 'PNG')
 
 
-def img_greyscale_normalize(img_src_dir, img_dst_dir):
+def img_greyscale_normalize(img_src_dir, img_dst_dir, pong=True):
     '''
     Run image preprocessing in 2 steps:
     i)  Turn into greyscale image
@@ -39,17 +39,12 @@ def img_greyscale_normalize(img_src_dir, img_dst_dir):
     for (_, _, filenames) in walk(img_src_dir):
         f = filenames
     print('Processing following images: ')
-    print(f)
-    mean_matrix = np.zeros((320, 210, 3))
+    mean_matrix = np.zeros(cv2.imread(img_src_dir + f[0]).shape)
     for file in f:
         img = cv2.imread(img_src_dir + file)
-        # img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        # img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
         mean_matrix += img
-        print('Img shape: {}'.format(img.shape))
-        # cv2.imwrite(img_dst_dir + file, img)
     mean_matrix /= len(f)
-    # print(mean_matrix)
+    print(mean_matrix)
     cv2.imwrite('./temp.png', mean_matrix)
     for file in f:
         img = cv2.imread(img_src_dir + file)
@@ -57,10 +52,24 @@ def img_greyscale_normalize(img_src_dir, img_dst_dir):
         img -= mean_matrix
         img[img > 255] = 255
         img[img < 0] = 0
+        if pong:
+            # Crop the top 30 pixels for pong score
+            for i in range(30):
+                img[i] = np.zeros((img.shape[1], 3))
+        else:
+            # Crop the top 40 pixels for tennis score
+            # for i in range(40):
+            #     img[i] = np.zeros((img.shape[1], 3))
+            img = img[40:(img.shape[0] - 20), 30:(img.shape[1] - 30)]
         img = img.astype(np.uint8)
-        print(img.shape)
         img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+        img = cv2.resize(img, (84, 84), interpolation=cv2.INTER_AREA)
+        if pong:
+            # Rotate the image
+            h, w = img.shape[0], img.shape[1]
+            M = cv2.getRotationMatrix2D((w / 2, h / 2), 90, 1.0)
+            img = cv2.warpAffine(img, M, (h, w)) 
         cv2.imwrite('./temp/' + file, img)
 
 
@@ -69,9 +78,16 @@ def random_sample_img(img_src_dir, img_dst_dir):
     for (_, _, filenames) in walk(img_src_dir):
         f = filenames
     print(f)
-    move_file = random.sample(f, 20)
-    for file in move_file:
-        shutil.move(img_src_dir + file, img_dst_dir + file)
+    tennis_file = list(filter(lambda x: x.startswith('tennis'), f))
+    pong_file = list(filter(lambda x: x.startswith('pong'), f))
+    # tennis_move_file = random.sample(tennis_file, 2100)
+    # pong_move_file = random.sample(pong_file, 2100)
+    for file in tennis_file:
+        shutil.move(img_src_dir + file, img_dst_dir + 'testA/' + file)
+    for file in pong_file:
+        shutil.move(img_src_dir + file, img_dst_dir + 'testB/' + file)
+    # for file in move_file:
+    #     shutil.move(img_src_dir + file, img_dst_dir + file)
 
 
 def file_rename(img_src_dir, name_prefix):
@@ -95,17 +111,18 @@ def list_file(img_src_dir):
 
 
 # rotate_pong_img(PONG_IMG_DIR)
-img_greyscale_normalize(PONG_IMG_DIR, OUTPUT_DIR)
+# img_greyscale_normalize(TENNIS_IMG_DIR, OUTPUT_DIR, False)
 # img_greyscale_normalize(PONG_IMG_DIR, OUTPUT_DIR)
 # random_sample_img('./datasets/atari/testB/', './datasets/atari/trainB/')
+# random_sample_img('./temp/', './datasets/atari/')
 # file_rename('./datasets/atari/testA/', 'tennis_test_')
 # file_rename('./datasets/atari/testB/', 'pong_test_')
 # file_rename('./datasets/atari/trainA/', 'tennis_train_')
 # file_rename('./datasets/atari/trainB/', 'pong_train_')
-# list_file('./datasets/atari/testA/')
-# print('----')
-# list_file('./datasets/atari/testB/')
-# print('----')
-# list_file('./datasets/atari/trainA/')
-# print('----')
-# list_file('./datasets/atari/trainB/')
+list_file('./datasets/atari/testA/')
+print('----')
+list_file('./datasets/atari/testB/')
+print('----')
+list_file('./datasets/atari/trainA/')
+print('----')
+list_file('./datasets/atari/trainB/')
