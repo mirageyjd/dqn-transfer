01623315edce78c0c216d68d057f9e25c646288e
Shiyu Wu <shiyuwu@umich.edu>
2019-12-03 20:59:35 -0600

    Add preprocess of removing background

--- run_img_preprocess.py
+++ run_img_preprocess.py
@@ -4,10 +4,11 @@ from os import walk
 import re
 import random
 import shutil
+import numpy as np
 
 
-TENNIS_IMG_DIR = './tennis_img/'
-PONG_IMG_DIR = './pong_img/'
+TENNIS_IMG_DIR = './datasets/atari/testA/'
+PONG_IMG_DIR = './datasets/atari/testB/'
 OUTPUT_DIR = './temp/'
 
 
@@ -39,11 +40,28 @@ def img_greyscale_normalize(img_src_dir, img_dst_dir):
         f = filenames
     print('Processing following images: ')
     print(f)
+    mean_matrix = np.zeros((320, 210, 3))
     for file in f:
         img = cv2.imread(img_src_dir + file)
+        # img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        mean_matrix += img
+        print('Img shape: {}'.format(img.shape))
+        # cv2.imwrite(img_dst_dir + file, img)
+    mean_matrix /= len(f)
+    # print(mean_matrix)
+    cv2.imwrite('./temp.png', mean_matrix)
+    for file in f:
+        img = cv2.imread(img_src_dir + file)
+        img = img.astype(float)
+        img -= mean_matrix
+        img[img > 255] = 255
+        img[img < 0] = 0
+        img = img.astype(np.uint8)
+        print(img.shape)
         img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
-        cv2.imwrite(img_dst_dir + file, img)
+        cv2.imwrite('./temp/' + file, img)
 
 
 def random_sample_img(img_src_dir, img_dst_dir):
@@ -77,7 +95,7 @@ def list_file(img_src_dir):
 
 
 # rotate_pong_img(PONG_IMG_DIR)
-# img_greyscale_normalize(TENNIS_IMG_DIR, OUTPUT_DIR)
+img_greyscale_normalize(PONG_IMG_DIR, OUTPUT_DIR)
 # img_greyscale_normalize(PONG_IMG_DIR, OUTPUT_DIR)
 # random_sample_img('./datasets/atari/testB/', './datasets/atari/trainB/')
 # file_rename('./datasets/atari/testA/', 'tennis_test_')
