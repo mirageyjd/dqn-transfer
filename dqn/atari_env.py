3ae7aeb6e92b3b538d3981852175f862d2f095d0
Jinde Yang <jinde.yang97@gmail.com>
2019-12-05 22:55:22 -0600

    update structure of code

---
+++ dqn/atari_env.py
@@ -0,0 +1,121 @@
+import gym
+import cv2
+import numpy as np
+
+
+def obs_preprocess(unwrapped_obs: np.ndarray) -> np.ndarray:
+    # convert into greyscale image
+    _obs = cv2.cvtColor(unwrapped_obs, cv2.COLOR_RGB2GRAY)
+    # scale into 84 * 84
+    _obs = cv2.resize(_obs, (84, 84), interpolation=cv2.INTER_AREA)
+    return _obs
+
+
+def clipped_reward(reward: float) -> float:
+    if reward > 0:
+        return 1.0
+    elif reward < 0:
+        return -1.0
+    else:
+        return 0.0
+
+
+# Wrapper of OpenAI gym env class targeted for Atari Game
+# Settings are from https://github.com/openai/baselines
+class AtariEnv(gym.Wrapper):
+    def __init__(self, env: gym.Env, config: dict):
+        gym.Wrapper.__init__(self, env)
+
+        self.history_len = config['history_len']
+        self.action_repeat = config['action_repeat']
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(self.history_len, 84, 84), dtype=np.uint8)
+
+        if config['action_mapping_on']:
+            self.action_space = gym.spaces.Discrete(len(config['action_mapping']))
+            self.action_mapping = np.zeros(len(config['action_mapping']), dtype=np.int)
+            action_meaning = self.env.unwrapped.get_action_meanings()
+            for i, action_name in enumerate(config['action_mapping']):
+                self.action_mapping[i] = action_meaning.index(action_name)
+        else:
+            self.action_mapping = np.arange(self.action_space.n)
+
+        self.no_op_max = config['no_op_max']
+
+    def reset(self, **kwargs):
+        # no-op reset
+        unwrapped_obs = self.env.reset(**kwargs)
+
+        num_no_op = np.random.randint(self.history_len - 1, self.no_op_max + 1)
+        obs = np.zeros((self.history_len, 84, 84))
+        if num_no_op == self.history_len - 1:
+            obs[self.history_len - 1] = obs_preprocess(unwrapped_obs)
+
+        for i in range(1, num_no_op + 1):
+            unwrapped_obs, reward, done, info = self.env.step(0)
+            if done:
+                return self.reset(**kwargs)
+
+            index = num_no_op - i
+            if index <= self.history_len - 1:
+                obs[index] = obs_preprocess(unwrapped_obs)
+
+        return obs
+
+    def step(self, action):
+        # Repeat taking same action
+        action = self.action_mapping[action]
+        obs = np.zeros((self.history_len, 84, 84))
+        total_reward = 0.0
+        done = False
+        info = None
+        for i in range(1, self.action_repeat + 1):
+            unwrapped_obs, reward, done, info = self.env.step(action)
+
+            index = self.action_repeat - i
+            if index <= self.history_len - 1:
+                obs[index] = obs_preprocess(unwrapped_obs)
+            total_reward += reward
+
+            if done:
+                break
+
+        return obs, clipped_reward(total_reward), done, info
+
+
+# Special wrapper for atari tennis: terminate when the first game ends
+class AtariTennisWrapper(gym.Wrapper):
+    def __init__(self, env: gym.Env):
+        gym.Wrapper.__init__(self, env)
+
+        self.player_score = 0
+        self.opponent_score = 0
+
+    def reset(self, **kwargs):
+        obs = self.env.reset(**kwargs)
+
+        self.player_score = 0
+        self.opponent_score = 0
+
+        return obs
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+
+        if reward != 0:
+            if reward > 0:
+                self.player_score += 1
+            else:
+                self.opponent_score += 1
+
+            done = self.game_terminate()
+
+        return obs, reward, done, info
+
+    def game_terminate(self):
+        if self.player_score >= 4 and self.player_score - self.opponent_score >= 2:
+            return True
+
+        if self.opponent_score >= 4 and self.opponent_score - self.player_score >= 2:
+            return True
+
+        return False
---
+++ dqn/train_agent.py
@@ -0,0 +1,78 @@
+from typing import Tuple
+import gym
+import torch
+from agent import Agent
+from replay_buffer import ReplayBuffer
+from logger import Logger
+from tqdm import tqdm
+
+
+def train_agent(env: gym.Env, agent: Agent, replay_buffer: ReplayBuffer, logger: Logger, config: dict):
+    start_t = 0
+    if config['recover']:
+        start_t = max(start_t, config['recover_t'] - config['learning_start'])
+        config['learning_start'] += start_t
+        agent.load_model_from_state_dict(torch.load(config['model_path']))
+        print('Load model from ', config['model_path'])
+        print('Recover training from step ', config['recover_t'])
+
+    s = env.reset()
+    for t in tqdm(range(start_t + 1, config['t_max'] + 1)):
+        # sampling from environment
+        epsilon = config['eps_start'] - (config['eps_start'] - config['eps_end']) * (t - 1) / (
+                    config['eps_end_t'] - 1) if t <= config['eps_end_t'] else config['eps_end']
+        a = agent.action(s, epsilon)
+        s2, r, done, _ = env.step(a)
+        replay_buffer.insert((s, a, r, s2, done))
+
+        if t > config['learning_start']:
+            if t % config['update_freq'] == 0:
+                # sample a mini-batch from replay buffer and update q function
+                s_batch, a_batch, r_batch, s2_batch, done_batch = replay_buffer.sample(config['batch_size'])
+                agent.train(s_batch, a_batch, r_batch, s2_batch, done_batch, config['gamma'])
+
+            if t % config['target_update_freq'] == 0:
+                # update target q function
+                agent.update_target()
+
+            if t % config['eval_freq'] == 0:
+                avg_reward, num_episode = eval_agent(env, agent, config)
+                logger.record(t, avg_reward, num_episode)
+                done = True
+
+            if t % config['checkpoint_freq'] == 0:
+                logger.save_model(agent.get_model())
+
+        s = env.reset() if done else s2
+
+    logger.train_over()
+
+
+def eval_agent(env: gym.Env, agent: Agent, config: dict) -> Tuple[float, int]:
+    total_reward = 0.0
+    num_episode = 0
+
+    s = env.reset()
+    done = False
+    for t in range(config['eval_t']):
+        a = agent.action(s, config['eval_eps'])
+        s2, r, done, _ = env.step(a)
+        total_reward += r
+
+        if done:
+            s = env.reset()
+            num_episode += 1
+        else:
+            s = s2
+
+    if not done:
+        if config['eval_complete_episode']:
+            while not done:
+                a = agent.action(s, config['eval_eps'])
+                s2, r, done, _ = env.step(a)
+                total_reward += r
+                s = s2
+
+        num_episode += 1
+
+    return total_reward / num_episode, num_episode
